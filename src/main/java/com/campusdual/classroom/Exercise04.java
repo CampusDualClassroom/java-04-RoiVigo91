f529f18b7aadf7c3b35e8398aabe736e09ae3bdc
Roi Oliveira Outeda <roioliveira@gmail.com>
2024-10-24 20:20:25 +0200

    Ejercicio04Solucionado

--- src/main/java/com/campusdual/classroom/Exercise04.java
+++ src/main/java/com/campusdual/classroom/Exercise04.java
@@ -2,29 +2,49 @@ package com.campusdual.classroom;
 
 public class Exercise04 {
     public static void main(String[] args) {
+        sumNum(0);
 
-        System.out.println("Ejemplos de cálculos SIN estructuras de control:");
-        System.out.println("Suma de los primeros 5 números: " + (1 + 2 + 3 + 4 + 5));
-        System.out.println("Suma de los primeros 20 números pares: " + (2 + 4 + 6 + 8 + 10 + 12 + 14 + 16 + 18 + 20 + 22 + 24 + 26 + 28 + 30 + 32 + 34 + 36 + 38 + 40));
-        System.out.println("Obtén el factorial del número 7 → (7!): " + (7 * 6 * 5 * 4 * 3 * 2 * 1));
+        System.out.println("Ejemplos de cálculos SIN estructuras de control:" + (1 + 2 + 3 + 4 + 5));
+        System.out.println("Suma de los primeros 5 números: " + sumNum(5));
+        System.out.println("Suma de los primeros 20 números pares: " + sumEvenNum(20));
+        System.out.println("Obtén el factorial del número 7 → (7!): " + factorial(7));
         System.out.println("######################################################################");
-        System.out.println("Ejemplos de cálculos CON estructuras de control:");
+        System.out.println("Ejemplos de cálculos CON estructuras de control: " + recursiveFactorial(7));
 
     }
 
     public static int sumNum(int num) {
-        return 0;
+
+        int suma =0 ;
+        for (int i = 1; i <= num; i++ ){
+            suma += i;
+        }
+        return suma;
     }
 
     public static int sumEvenNum(int num) {
-        return 0;
+        int suma2 = 0;
+
+        for(int i = 1; i <= num; i++){
+            suma2 += i * 2;
+        }
+        return suma2;
+
     }
 
     public static int factorial(int num) {
-        return 0;
+        int factorial1 = 1;
+        for(int i = 1; i<= num ;i++){
+            factorial1 *= i;
+        }
+        return factorial1;
     }
 
     public static int recursiveFactorial(int num) {
-        return 0;
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * recursiveFactorial(num - 1);
+        }
     }
 }
